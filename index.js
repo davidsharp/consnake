e62ee5a9c1e706b3674d99e7164e0386133d7a6b
David Sharp <hello@davidsharp.codes>
2022-10-19 18:06:57 +0100

    initial commit, detects console opening and inits "game"

---
+++ index.js
@@ -0,0 +1,17 @@
+import devtools from './node_modules/devtools-detect/index.js';
+
+let inited = false
+
+const start = () => {
+  inited = true
+  console.log('inited!')
+}
+
+const init = () => {
+  if(devtools.isOpen) start();
+  else window.addEventListener('devtoolschange', event => {
+    if(!inited && event.detail.isOpen) start()
+  })
+}
+
+export default init
